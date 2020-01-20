3f89ec64c1896030ec10012537115f8897f2b90d
Ikechukwu Madu <ikechukwu.madu1@gmail.com>
2020-01-20 22:56:07 +0100

    Added more functionality to category handler

--- handlers/category.go
+++ handlers/category.go
@@ -2,34 +2,69 @@ package handlers
 
 import (
 	"context"
+	"net/http"
 	"time"
 
-	"go.mongodb.org/mongo-driver/mongo"
-
-	"github.com/imadu/e-commerce-api/db/models"
 	"github.com/labstack/echo"
 	"github.com/labstack/gommon/log"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+
+	"github.com/imadu/cakes-and-cream/db/models"
 )
 
+var client *mongo.Client
+var collection = client.Database("cakes-and-cream-go").Collection("categories")
+
 // CreateNewCategory function
-func CreateNewCategory(c echo.Context, client *mongo.Client) {
+func CreateNewCategory(c echo.Context) error {
 	r := new(models.Category)
 	if err := c.Bind(r); err != nil {
 		log.Errorf("Could not bind request to struct: %+v", err)
 		return sendError(c, "", "", "")
 	}
-	collection := client.Database("cakes-and-cream-go").Collection("category")
-	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
 	result, _ := collection.InsertOne(ctx, r)
-	return sendSuccess(c, nil)
+	return sendSuccess(c, result.InsertedID)
 }
 
 // GetCategories Function
-func GetCategories(c echo.Context, client *mongo.Client) []models.Category {
-	var categories []models.Category
-	collection := client.Database("cakes-and-cream-go").Collection("category")
-	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
-	result, _ := collection.Find(&categories, ctx)
-	return sendData(c, categories)
+func GetCategories(c echo.Context) error {
+	findOptions := options.Find()
+	findOptions.SetLimit(10)
+	var categories []*models.Category
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	result, err := collection.Find(ctx, bson.D{{}}, findOptions)
+	if err != nil {
+		c.Response().WriteHeader(http.StatusNotFound)
+		return sendError(c, "", "Categories do not exist", "")
+	}
 
+	for result.Next(context.TODO()) {
+		var category models.Category
+		err := result.Decode(&category)
+		if err != nil {
+			log.Errorf("something went wrong", err)
+		}
+		categories = append(categories, &category)
+	}
+
+	return sendData(c, categories)
+}
+
+// GetCategory function
+func GetCategory(c echo.Context) error {
+	id := c.Param("id")
+	var result models.Category
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	err := collection.FindOne(ctx, id).Decode(&result)
+	if err != nil {
+		c.Response().WriteHeader(http.StatusNotFound)
+		return sendError(c, "", "Category do not exist", "")
+	}
+	return sendData(c, result)
 }
